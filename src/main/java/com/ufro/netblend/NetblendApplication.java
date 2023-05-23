b69a179ff4148c5dccfb788b992c178b54a04cc9
vicktorl <v.lenam01@ufromail.cl>
2023-05-23 14:52:36 -0400

    se sube proyecto

---
+++ src/main/java/com/ufro/netblend/NetblendApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.netblend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetblendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetblendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufro/netblend/NetblendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufro.netblend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetblendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
